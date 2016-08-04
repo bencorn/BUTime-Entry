ad64e2346ee2ff97592fc768f197f41fb1465b3a
Benjamin Corn <bcorn@bu.edu>
2016-08-03 22:04:30 -0400

    Background Updates

---
+++ background.js
@@ -0,0 +1,147 @@
+﻿var response = {};
+
+chrome.storage.sync.get(null, function (obj) {
+    response = obj;
+
+    // Begin Monday 
+
+    var dp = $("tr:contains('Monday')")[0].cells[2].children[0];
+    var select = $(dp).find("select");
+    $(select).val(response.Monday["Begin1"]);
+
+    dp = $("tr:contains('Monday')")[0].cells[3].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Monday["End1"]);
+
+    dp = $("tr:contains('Monday')")[0].cells[4].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Monday["Begin2"]);
+
+    dp = $("tr:contains('Monday')")[0].cells[5].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Monday["End2"]);
+
+    dp = $("tr:contains('Monday')")[0].cells[6].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Monday["Begin3"]);
+
+    dp = $("tr:contains('Monday')")[0].cells[7].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Monday["End3"]);
+
+    // End Monday
+
+    // Begin Tuesday
+
+    dp = $("tr:contains('Tuesday')")[0].cells[2].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Tuesday["Begin1"]);
+
+    dp = $("tr:contains('Tuesday')")[0].cells[3].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Tuesday["End1"]);
+
+    dp = $("tr:contains('Tuesday')")[0].cells[4].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Tuesday["Begin2"]);
+
+    dp = $("tr:contains('Tuesday')")[0].cells[5].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Tuesday["End2"]);
+
+    dp = $("tr:contains('Tuesday')")[0].cells[6].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Tuesday["Begin3"]);
+
+    dp = $("tr:contains('Tuesday')")[0].cells[7].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Tuesday["End3"]);
+
+    // End Tuesday
+
+    // Begin Wednesday
+
+    dp = $("tr:contains('Wednesday')")[0].cells[2].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Wednesday["Begin1"]);
+
+    dp = $("tr:contains('Wednesday')")[0].cells[3].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Wednesday["End1"]);
+
+    dp = $("tr:contains('Wednesday')")[0].cells[4].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Wednesday["Begin2"]);
+
+    dp = $("tr:contains('Wednesday')")[0].cells[5].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Wednesday["End2"]);
+
+    dp = $("tr:contains('Wednesday')")[0].cells[6].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Wednesday["Begin3"]);
+
+    dp = $("tr:contains('Wednesday')")[0].cells[7].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Wednesday["End3"]);
+
+    // End Wednesday
+
+    // Begin Thursday
+
+    dp = $("tr:contains('Thursday')")[0].cells[2].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Thursday["Begin1"]);
+
+    dp = $("tr:contains('Thursday')")[0].cells[3].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Thursday["End1"]);
+
+    dp = $("tr:contains('Thursday')")[0].cells[4].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Thursday["Begin2"]);
+
+    dp = $("tr:contains('Thursday')")[0].cells[5].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Thursday["End2"]);
+
+    dp = $("tr:contains('Thursday')")[0].cells[6].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Thursday["Begin3"]);
+
+    dp = $("tr:contains('Thursday')")[0].cells[7].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Thursday["End3"]);
+
+    // End Thursday
+
+    // Begin Friday
+
+    dp = $("tr:contains('Friday')")[0].cells[2].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Friday["Begin1"]);
+
+    dp = $("tr:contains('Friday')")[0].cells[3].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Friday["End1"]);
+
+    dp = $("tr:contains('Friday')")[0].cells[4].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Friday["Begin1"]);
+
+    dp = $("tr:contains('Friday')")[0].cells[5].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Friday["End2"]);
+
+    dp = $("tr:contains('Friday')")[0].cells[6].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Friday["Begin3"]);
+
+    dp = $("tr:contains('Friday')")[0].cells[7].children[0];
+    select = $(dp).find("select");
+    $(select).val(response.Friday["End3"]);
+
+    // End Friday
+
+});
+
--- timeEntryController.js
+++ timeEntryController.js
@@ -9,22 +9,30 @@
     // code for controller itself
     function timeEntryController($http) {
 
+        chrome.storage.sync.get(null, function (obj) {
+            var response = obj;
+            vm.Monday = response.Monday;
+            vm.Tuesday = response.Tuesday;
+            vm.Wednesday = response.Wednesday;
+            vm.Thursday = response.Thursday;
+            vm.Friday = response.Friday;
+        });
+
         var vm = this;
 
-        vm.save_options = function() {
+        vm.save_options = function () {
             chrome.storage.sync.set({
-                Monday: vm.Monday,
-                Tuesday: vm.Tuesday,
-                Wednesday: vm.Wednesday,
-                Thursday: vm.Thursday,
-                Friday: vm.Friday
+                "Monday": vm.Monday,
+                "Tuesday": vm.Tuesday,
+                "Wednesday": vm.Wednesday,
+                "Thursday": vm.Thursday,
+                "Friday": vm.Friday
             }, function () {
-                // Update status to let user know options were saved.
-                alert(chrome.storage.get("Monday", function () {
-                }));
-                setTimeout(function () {
-                    status.textContent = '';
-                }, 750);
+                vm.Response = "Changes saved successfully."
+
+                chrome.tabs.executeScript({
+                    file: "background.js"
+                });
             });
         }
 
